9c562556feeabcb3bacedb6cdcf9117b6be8a22d
NicoFernandezM <n.fernandez07@ufromail.cl>
2023-03-28 13:07:00 -0300

    agrego metodo 1

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,9 @@
+public class Launcher {
+    public static void main(String[] args) {
+        metodo1();
+    }
+
+    private static void metodo1() {
+        System.out.println("Soy el método 1.");
+    }
+}
