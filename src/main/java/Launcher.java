db0e3b608b7a940e05e1a265744b9cb9fe4ac774
NicoFernandezM <n.fernandez07@ufromail.cl>
2023-03-28 13:08:32 -0300

    Agrego método final

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,9 +1,14 @@
 public class Launcher {
     public static void main(String[] args) {
         metodo1();
+        metodoFinal();
     }
 
     private static void metodo1() {
         System.out.println("Soy el método 1.");
+    }
+
+    private static void metodoFinal() {
+        System.out.println("Soy el método final.");
     }
 }
